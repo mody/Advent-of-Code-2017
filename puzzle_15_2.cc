9f8251a43e8c191bb7c7d5b80b6348a77fb11e37
Patrik Modesto <patrik.modesto@firma.seznam.cz>
2017-12-15 11:03:35 +0100

    Puzzle 15 - part 2

---
+++ puzzle_15_2.cc
@@ -0,0 +1,36 @@
+// Copyright (C) 2017, Seznam.cz, a.s.
+
+#include <iostream>
+#include <limits>
+
+int main()
+{
+    const unsigned long MULA = 16807;
+    const unsigned long MULB = 48271;
+    unsigned long genA = 722;
+    unsigned long genB = 354;
+    int same = 0;
+
+    for (int i=0; i < 5000000; ) {
+        for(;;) {
+            genA = (genA * MULA) % 2147483647U;
+            if (((genA / 4) * 4) == genA) {
+                break;
+            }
+        }
+        for(;;) {
+            genB = (genB * MULB) % 2147483647U;
+            if (((genB / 8) * 8) == genB) {
+                break;
+            }
+        }
+
+        if ((genA & 0xffff) == (genB & 0xffff)) {
+            ++same;
+        }
+        ++i;
+    }
+
+    std::cout << "same: " << same << "\n";
+    return 0;
+}
