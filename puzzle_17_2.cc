70950a5b279d0aab5dc5ba065e6f8b3024cb2d38
Patrik Modesto <patrik.modesto@firma.seznam.cz>
2017-12-17 14:47:14 +0100

    Puzzle 17 - part 2

---
+++ puzzle_17_2.cc
@@ -0,0 +1,20 @@
+// Copyright (C) 2017, Seznam.cz, a.s.
+
+#include <iostream>
+
+constexpr int STEPS = 376;
+constexpr int ITERATIONS = 50000001;
+
+int main()
+{
+    int answer_pt2 = 0;
+
+    for (int i = 1, idx = 0; i < ITERATIONS; i++) {
+        idx = (idx + 1 + STEPS) % i;
+        if (idx == 0) answer_pt2 = i;
+    }
+
+    std::cout << answer_pt2 << std::endl;
+
+    return 0;
+}
